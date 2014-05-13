91b9c7d32c04d9de80a2d311367a38bd594fc065
Guillaume Wenzek <guillaume.wenzek@polytechnique.org>
2014-05-13 16:32:56 +0200

    added first TD

---
+++ TD1/rpn.hs
@@ -0,0 +1,45 @@
+module RPN where
+
+import System.IO
+
+type Stack = [Int]
+
+type Operator = Stack -> Stack
+
+parseOp :: String -> Operator
+parseOp instruction = 
+    case instruction of 
+        "+"     ->  \(x : y : s)  ->  (x + y) : s
+        "-"     ->  \(x : y : s)  ->  (x - y) : s
+        "*"     ->  \(x : y : s)  ->  (x * y) : s
+        "/"     ->  \(x : y : s)  ->  (div x y) : s
+        "dup"   ->  \(x : s)  ->  x : x : s
+        "swap"  ->  \(x : y : s)  ->  y : x : s
+        "drop"  ->  \(x : s)  ->  s
+        "pick"  ->  \(n : s)  ->  (pick n (n : s)) : s
+        "depth" ->  \s  ->  (length s) : s
+        "quit" -> error "quitting"
+        int     ->  \s -> (read int :: Int) : s
+
+pick :: Int -> [a] -> a
+pick 0 (x : _) = x
+pick n (_ : s) = pick (n-1) s
+
+eval :: a -> [a -> a] -> a
+eval x [] = x
+eval x (f : fs) = eval (f x) fs
+
+parse :: String -> [Operator]
+parse instruction = map parseOp (words instruction)
+
+repl :: Stack -> IO ()
+repl stack = do
+  putStr "> "
+  hFlush stdout
+  line <- getLine
+  newstack <- return $ eval stack (parse line)
+  putStrLn $ show $ reverse newstack
+  repl newstack
+
+main = repl []
+
